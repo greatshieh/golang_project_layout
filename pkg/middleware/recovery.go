e342ff71d0979bba56610aa8ad06e18ba717d8f1
xiewei <ivan.xiewei@foxmail.com>
2023-04-18 16:39:18 +0800

    优化recovery中间件

--- pkg/middleware/recovery.go
+++ pkg/middleware/recovery.go
@@ -2,29 +2,65 @@ package middleware
 
 import (
 	"fmt"
-	"golang_project_layout/pkg/errcode"
 	"golang_project_layout/pkg/global"
-	"golang_project_layout/pkg/model/common/response"
 	"golang_project_layout/pkg/plugin/email"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"os"
+	"runtime/debug"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/marmotedu/errors"
+	"go.uber.org/zap"
 )
 
-func Recovery() gin.HandlerFunc {
+func Recovery(stack bool) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		defer func() {
 			if err := recover(); err != nil {
-				global.GVA_LOG.Error(fmt.Sprintf("panic recover err: %-v", err))
 				emailService := new(email.EmailService)
-				err := emailService.SendEmail(global.GVA_CONFIG.Email.To, fmt.Sprintf("异常发生. 时间: %s", time.Now().Local().Format("2006-01-02 15:04:05")), fmt.Sprintf("错误信息: %-v", err))
-				if err != nil {
-					response.WriteResponse(c, errors.WithCode(errcode.ErrEmailSend, err.Error()), nil)
-					c.Abort()
-				} else {
-					response.WriteResponse(c, errors.WithCode(errcode.ErrInternalServer, "内部异常"), nil)
+				// Check for a broken connection, as it is not really a
+				// condition that warrants a panic stack trace.
+				var brokenPipe bool
+				if ne, ok := err.(*net.OpError); ok {
+					if se, ok := ne.Err.(*os.SyscallError); ok {
+						if strings.Contains(strings.ToLower(se.Error()), "broken pipe") || strings.Contains(strings.ToLower(se.Error()), "connection reset by peer") {
+							brokenPipe = true
+						}
+					}
 				}
+
+				httpRequest, _ := httputil.DumpRequest(c.Request, false)
+				if brokenPipe {
+					global.GVA_LOG.Error(c.Request.URL.Path,
+						zap.Any("error", err),
+						zap.String("request", string(httpRequest)),
+					)
+					// If the connection is dead, we can't write a status to it.
+					_ = c.Error(err.(error)) // nolint: errcheck
+					c.Abort()
+				}
+
+				if stack && !brokenPipe {
+					global.GVA_LOG.Error("[Recovery from panic]",
+						zap.Any("error", err),
+						zap.String("request", string(httpRequest)),
+						zap.String("stack", string(debug.Stack())),
+					)
+				} else {
+					global.GVA_LOG.Error("[Recovery from panic]",
+						zap.Any("error", err),
+						zap.String("request", string(httpRequest)),
+					)
+				}
+
+				if !brokenPipe {
+					c.AbortWithStatus(http.StatusInternalServerError)
+				}
+
+				emailService.SendEmail(global.GVA_CONFIG.Email.To, fmt.Sprintf("异常发生. 时间: %s", time.Now().Local().Format("2006-01-02 15:04:05")), zap.Any("error", err).String)
 			}
 		}()
 		c.Next()
